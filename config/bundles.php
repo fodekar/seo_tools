9974429fb314c9ba7eecd3c39682d938f5606971
Hess KITOKO - NGOMA <hess.website@gmail.com>
2019-06-13 18:21:08 +0200

    Revert "[SEO_TOOLS-3077] merge master after integration admin CRUD"
    This reverts commit 95a6c432dd431fac9f14a56a56722c2f391e7a22.

--- config/bundles.php
+++ config/bundles.php
@@ -20,7 +20,4 @@ return [
     Sonata\BlockBundle\SonataBlockBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Sonata\AdminBundle\SonataAdminBundle::class => ['all' => true],
-    Sonata\EasyExtendsBundle\SonataEasyExtendsBundle::class => ['all' => true],
-    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
-    Sonata\MediaBundle\SonataMediaBundle::class => ['all' => true],
 ];
